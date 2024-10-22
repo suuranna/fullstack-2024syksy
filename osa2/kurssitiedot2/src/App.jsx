456b4de5aa7070ccf4f1d2d05d2fec814021f2b4
Anna Suuronen <annasuuronen@icloud.com>
2024-10-22 20:37:39 +0300

    tehtävä 2.4 tehty

--- osa2/kurssitiedot2/src/App.jsx
+++ osa2/kurssitiedot2/src/App.jsx
@@ -1,6 +1,6 @@
 const Header = ({ name }) => {
   return (
-    <h1>{name}</h1>
+    <h2>{name}</h2>
   )
 }
 
@@ -42,39 +42,70 @@ const Course = ({ course }) => {
   )
 }
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
+const Courses = ({ courses }) => {
+  const coursesMapped = courses.map(course =>
+    <Course course={course} key={course.id} />
+  )
   return (
     <div>
-      <Course course={course} />
+      {coursesMapped}
     </div>
   )
 }
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <Courses courses={courses} />
+    </div>
+  )
+}
+
 
 export default App
