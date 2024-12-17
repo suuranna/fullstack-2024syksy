801427b5fe0b11c1456a872710216285ae675e60
Anna Suuronen <annasuuronen@icloud.com>
2024-12-17 13:30:41 +0200

    tehtävä 6.9 tehty

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,27 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
+import Filter from "./Filter";
+import { filterChange } from "../reducers/filterReducer";
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
-  
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
+
   const vote = (id) => {
     dispatch(voteAnecdote(id))
+    dispatch(filterChange(filter))
   }
 
   return (
     <div>
-      {anecdotes.sort((first, second) => second.votes - first.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      <Filter />
+      {anecdotes.sort((first, second) => second.votes - first.votes)
+        .filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        )
+      }
     </div>
   )
 }
---
+++ osa6/redux-anecdotes/src/components/Filter.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux"
+import { filterChange } from "../reducers/filterReducer"
+
+const Filter = () => {
+  const dispatch = useDispatch()
+  const filter = useSelector(state => state.filter)
+  
+  const handleChange = (event) => {
+    // input-kentän arvo muuttujassa event.target.value
+    dispatch(filterChange(event.target.value))
+    console.log(filter)
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
