d92b312f8006beca59ba6ef954c2b9b57b8fe638
Anna Suuronen <annasuuronen@icloud.com>
2024-12-18 02:35:18 +0200

    tehtävä 6.16 tehty

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -3,13 +3,14 @@ import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import { useEffect } from 'react'
 import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes, setAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    //anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [])
 
   return (
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 /*const anecdotesAtStart = [
   'If it hurts, do it more often',
@@ -9,7 +10,7 @@ import { createSlice } from '@reduxjs/toolkit'
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]*/
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+/*const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
   return {
@@ -17,7 +18,7 @@ const asObject = (anecdote) => {
     id: getId(),
     votes: 0
   }
-}
+}*/
 
 //const initialState = anecdotesAtStart.map(asObject)
 
@@ -44,4 +45,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
