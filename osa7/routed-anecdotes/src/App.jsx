c2b0f977fdabc72f75ec667740348434368039d3
Anna Suuronen <annasuuronen@icloud.com>
2024-12-22 23:42:02 +0200

    tehtävä 7.5 tehty

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -90,6 +90,13 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = (event) => {
+    event.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -107,6 +114,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
