987bbd6d13f3161ce2e69576ddab11c8163d9cd3
Anna Suuronen <annasuuronen@icloud.com>
2024-12-22 23:51:19 +0200

    tehtävä 7.6 tehty

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -103,15 +103,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...{...content, reset: undefined}} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...{...author, reset: undefined}} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...{...info, reset: undefined}} />
         </div>
         <button>create</button>
         <button onClick={handleReset}>reset</button>
